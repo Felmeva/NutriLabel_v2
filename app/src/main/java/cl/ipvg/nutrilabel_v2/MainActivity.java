e1c0c2339b2153202c3ad85df6e8faa1b9c64933
Felmev <felipe.mella1@virginiogomez.cl>
2023-11-09 20:00:32 -0300

    nutrilabel

--- app/src/main/java/cl/ipvg/nutrilabel_v2/MainActivity.java
+++ app/src/main/java/cl/ipvg/nutrilabel_v2/MainActivity.java
@@ -3,6 +3,9 @@ package cl.ipvg.nutrilabel_v2;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
@@ -21,9 +24,33 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
+    private Button agregarBt, actuaBt, elimBt, genEtBt;
+    private EditText nombre, ing1, ing2, ing3, gram1, gram2, gram3, codNum;
+
+    private Spinner nomRec1, nomRec2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        agregarBt = (Button) findViewById(R.id.button3);
+        actuaBt = (Button)  findViewById(R.id.button2);
+        elimBt = (Button) findViewById(R.id.button4);
+        genEtBt = (Button) findViewById(R.id.button);
+
+        nombre = (EditText) findViewById(R.id.editTextText);
+        ing1 = (EditText) findViewById(R.id.editTextText7);
+        ing2 = (EditText) findViewById(R.id.editTextText8);
+        ing3 = (EditText) findViewById(R.id.editTextText9);
+        gram1 = (EditText) findViewById(R.id.editTextText10);
+        gram2 = (EditText) findViewById(R.id.editTextText11);
+        gram3 = (EditText) findViewById(R.id.editTextText12);
+        codNum = (EditText) findViewById(R.id.editTextText5);
+
+        nomRec1 = (Spinner) findViewById(R.id.spinner);
+        nomRec2 = (Spinner) findViewById(R.id.spinner4);
+
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
