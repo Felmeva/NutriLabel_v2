4a396e03ac54ea103a5ee287758c788f7c868d3e
felmeva <felipe.mella1@virginiogomez.cl>
2023-11-15 10:19:02 -0300

    NutriLabel

--- app/src/main/java/cl/ipvg/nutrilabel_v2/MainActivity.java
+++ app/src/main/java/cl/ipvg/nutrilabel_v2/MainActivity.java
@@ -156,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "En preparación", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
