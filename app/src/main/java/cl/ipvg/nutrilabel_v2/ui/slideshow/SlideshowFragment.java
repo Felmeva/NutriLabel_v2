739142c99e5cb5ba4e2f60478b0da6786fdea58e
Felmev <felipe.mella1@virginiogomez.cl>
2023-11-16 20:36:27 -0300

    nutrilabel

--- app/src/main/java/cl/ipvg/nutrilabel_v2/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/cl/ipvg/nutrilabel_v2/ui/slideshow/SlideshowFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import cl.ipvg.nutrilabel_v2.databinding.FragmentSlideshowBinding;
 
@@ -26,6 +29,16 @@ public class SlideshowFragment extends Fragment {
 
         final TextView textView = binding.textSlideshow;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final Button btgenerar = binding.button;
+
+        btgenerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "boton generar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         return root;
     }
 
